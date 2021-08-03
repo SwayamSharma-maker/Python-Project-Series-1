074c7eef9ae2a82cc855308b2578cecd4d53bf45
Swayam Sharma <jr.swayam@gmail.com>
2021-08-03 13:20:01 +0530

    Add files via upload

---
+++ Python Project 1.py
@@ -0,0 +1,64 @@
+score = 0
+
+print(" ")
+print("Rakshabandhan Special Quiz")
+print(" ")
+
+print("Which other religion celebrates Raksha bandhan apart from Hinduism?")
+print("1. Buddhism  2. Christanity  3. Jainism")
+print(" ")
+
+c = input("What's your choice? Enter the option number! ")
+if c == "3":
+    print("Hey, correct answer!")
+    score += 1
+else:
+    print("Sorry, better luck next time! The correct answer was 'Jainism'")
+
+print(" ")
+
+
+print("On what date does Raksha Bandhan fall according to Hindi Calendar?")
+print("1. Last day of Shravan  2. First day of Kartik  3. Gyaras of Kartik")
+print(" ")
+
+c = input("What's your choice? Enter the option number! ")
+if c == "1":
+    print("Hey, correct answer!")
+    score += 1
+else:
+    print("Sorry, better luck next time! The correct answer was 'Last day of Shravan'")
+
+print(" ")
+
+
+print("What is the name of the rakhis tied by sister to sister-in-law?")
+print("1. Rumba Rakhi  2. Lumba Rakhi  3. Tumba Rakhi")
+print(" ")
+
+c = input("What's your choice? Enter the option number! ")
+if c == "2":
+    print("Hey, correct answer!")
+    score += 1
+else:
+    print("Sorry, better luck next time! The correct answer was 'Lumba Rakhi'")
+
+print(" ")
+
+
+print("Who was given boon by Lord Vishnu that He would stay at his place and whom did Maa Lakshmi") 
+print("tie a rakhi to let Lord Vishnu stay most part of the year at sheersagar?")
+print("1. Danavraj Bali  2. Raja Krishnadevray  3. Raja Bodhisata")
+print(" ")
+
+c = input("What's your choice? Enter the option number! ")
+if c == "1":
+    print("Hey, correct answer!")
+    score += 1
+else:
+    print("Sorry, better luck next time! The correct answer was 'Danavraj Bali'")
+
+print(" ")
+
+
+print(f"Thank you for playing this quiz! Your score was {score} out of 4! Cheers!")
